cae359b7ca944373b95aa81f1abd4e0d2ae9a175
zhentian-wan <answer881215@gmail.com>
2020-09-24 21:34:10 +0300

    fix error

--- src/data.ts
+++ src/data.ts
@@ -4,25 +4,25 @@ export const defTimeTable: IWeeklyTimeTable = {
   monday: [],
   tuseday: [
     { type: "open", value: 36000 },
-    { type: "close", value: 64800 }
+    { type: "close", value: 64800 },
   ],
   wednesday: [],
   thursday: [
     { type: "open", value: 36000 },
-    { type: "close", value: 64800 }
+    { type: "close", value: 64800 },
   ],
-  friday: [{ type: "open", value: 36000 }],
+  firday: [{ type: "open", value: 36000 }],
   saturday: [
     { type: "close", value: 3600 },
     { type: "open", value: 32400 },
     { type: "close", value: 39600 },
-    { type: "open", value: 57600 }
+    { type: "open", value: 57600 },
   ],
   sunday: [
     { type: "close", value: 3600 },
     { type: "open", value: 43200 },
-    { type: "close", value: 75600 }
-  ]
+    { type: "close", value: 75600 },
+  ],
 };
 
 export async function getTimeTable() {
